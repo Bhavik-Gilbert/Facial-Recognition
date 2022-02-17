857d7fe32a9cef3974781e67abc44ca23ef7934d
Bhavik-Gilbert <bhaviklob@gmail.com>
2022-02-17 23:01:06 +0000

    CLEANUP

--- connection.py
+++ connection.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mysql.connector
 from mysql.connector import Error
 
@@ -31,38 +30,4 @@ def execute_query(connection, query, record=""):
         print(f"The error '{e}' occurred")
 
 def query(query: str, record=True):
-=======
-import mysql.connector
-from mysql.connector import Error
-
-def create_connection(host_name="localhost", user_name="root", user_password="", db_name="facial_recogntion"):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=db_name
-        )
-        print("Connection to MySQL DB successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-    return connection
-
-def execute_query(connection, query, record=""):
-    cursor = connection.cursor(buffered=True)
-    try:
-        if(record == ""):
-            cursor.execute(query)
-        else:
-            cursor.execute(query, record)
-        connection.commit()
-        print("Query executed successfully")
-        return cursor.fetchall()
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-def query(query: str, record=True):
->>>>>>> e33e4c3e5b289d3b579924e9fc6f92ab5e917a8d
     return execute_query(create_connection(), query, record)
